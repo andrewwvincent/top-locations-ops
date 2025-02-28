a4f1fd5b0fb076a87db87b0da78ece1ff099dfe3
andrewwvincent <andrew.vincent@trilogy.com>
2025-02-28 15:58:45 -0500

    Cleaned up many excess files and fixed OKC's missing tile

--- scripts/convert_points_to_tiles.py
+++ scripts/convert_points_to_tiles.py
@@ -124,7 +124,8 @@ def process_csv_files(input_dir, output_dir):
     print("Conversion complete!")
 
 if __name__ == "__main__":
-    input_dir = r"c:\Users\andre\OneDrive\Documents\Trilogy-Andrew-Desktop\Demo Heatmaps\Convert Points to Area\Final Heatmaps\data\demographics"
-    output_dir = r"c:\Users\andre\OneDrive\Documents\Trilogy-Andrew-Desktop\Demo Heatmaps\Convert Points to Area\Final Heatmaps\data\tiles"
+    input_dir = r"c:\Users\andre\OneDrive\Documents\Trilogy-Andrew-Desktop\Demo Heatmaps\Convert Points to Area\Final Heatmaps\data\High-Demographics"
+    output_dir = r"c:\Users\andre\OneDrive\Documents\Trilogy-Andrew-Desktop\Demo Heatmaps\Convert Points to Area\Final Heatmaps\data"
     
     process_csv_files(input_dir, output_dir)
+    print("Conversion complete!")
